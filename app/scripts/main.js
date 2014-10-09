99d11b64043051b05cccad3c9b792b6f51614e14
Max Ehnert <max.ehnert@gmail.com>
2014-10-09 14:37:45 -0400

    old style js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,36 @@
+// var user= 'maxehnert';
+//
+// var url = 'https://api.github.com/users/' + user + '?b21ce09df6bf6f0aaefa2bc538eaa28eaf7e9dd7';
+//
+// var template= $('#user_info').html();
+// var rendered = _.template(template);
+//
+// var test = $.getJSON(url).done(function(user_data){
+// //enter in here as many statements that pertain to this 'user_data' as possible
+//
+// $('.hero-unit').append(rendered(user_data));
+//
+//
+// });
+//
+// /////////// HTML //////////////
+//   <div class="hero-unit">
+//       <h1>'Allo, 'Allo!</h1>
+//
+//     <script type="text/template" id="user_info">
+//       <img src="<%= avatar_url%>" style="width:150px"/>
+//       <ul>
+//       <li><%= name %></li>
+//
+//       <li>User Name <%= login %></li>
+//     </script>
+//           </ul>
+//
+//     </div>
+
+///**********************************************///
+
+
 //Inserting My Name into the sidebar
 var temp_string = $('#name').html();
 var rendered_temp = _.template(temp_string);
