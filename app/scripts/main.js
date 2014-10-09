a5adb653d4407b2ea4b4dcddb37fdf9257bcf3d1
Max Ehnert <max.ehnert@gmail.com>
2014-10-09 09:13:31 -0400

    added more scripts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,3 +19,34 @@ var rendered_temp = _.template(temp_string);
 
 
   $('.username').append(rendered_temp(user));
+
+
+//Inserting My Location into the sidebar
+var temp_string = $('#location').html();
+var rendered_temp = _.template(temp_string);
+
+
+  $('.my-location').append(rendered_temp(user));
+
+
+//Inserting My Join Date into the sidebar
+var temp_string = $('#join-date').html();
+var rendered_temp = _.template(temp_string);
+
+
+  $('.my-join-date').append(rendered_temp(user));
+
+
+//Inserting Followers into the sidebar
+var temp_string = $('#followers').html();
+var rendered_temp = _.template(temp_string);
+
+
+  $('.followers').append(rendered_temp(user));
+
+//Inserting Following into the sidebar
+var temp_string = $('#following').html();
+var rendered_temp = _.template(temp_string);
+
+
+  $('.following').append(rendered_temp(user));
