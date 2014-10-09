458fd90eada595060bf015aee8b516d665965212
Max Ehnert <max.ehnert@gmail.com>
2014-10-09 18:41:06 -0400

    avatars sized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,20 @@
-// var user= 'maxehnert';
-//
-// var url = 'https://api.github.com/users/' + user + '?b21ce09df6bf6f0aaefa2bc538eaa28eaf7e9dd7';
-//
-// var template= $('#user_info').html();
-// var rendered = _.template(template);
-//
+var user= 'maxehnert';
+
+var url = 'https://api.github.com/users/' + user + '?b21ce09df6bf6f0aaefa2bc538eaa28eaf7e9dd7';
+
+//var template= $('#user_info').html();
+//var rendered = _.template(template);
+
 // var test = $.getJSON(url).done(function(user_data){
 // //enter in here as many statements that pertain to this 'user_data' as possible
 //
-// $('.hero-unit').append(rendered(user_data));
-//
+// // example~~~~> $('.hero-unit').append(rendered(user_data));
+// $('.my-name').append(rendered_temp(user_data));
+// $('.username').append(rendered_temp(user));
+// $('.my-location').append(rendered_temp(user));
+// $('.my-join-date').append(rendered_temp(user));
+// $('.followers').append(rendered_temp(user));
+// $('.following').append(rendered_temp(user));
 //
 // });
 //
@@ -30,56 +35,58 @@
 
 ///**********************************************///
 
+//Inserting My Avatar into the sidebar
+var temp_string1 = $('#avatar').html();
+var rendered_temp1 = _.template(temp_string1);
+
 
 //Inserting My Name into the sidebar
-var temp_string = $('#name').html();
-var rendered_temp = _.template(temp_string);
+var temp_string2 = $('#name').html();
+var rendered_temp2 = _.template(temp_string2);
 
 
-  $('.my-name').append(rendered_temp(user));
-
-//Inserting My Avatar into the sidebar
-// var temp_string = $('#avatar').html();
-// var rendered_temp = _.template(temp_string);
-//
-//
-//   $('.avatar').append(rendered_temp(user));
-
-
-//Inserting My User Name into the sidebar
-var temp_string = $('#login').html();
-var rendered_temp = _.template(temp_string);
-
-
-  $('.username').append(rendered_temp(user));
+//Inserting My User Name into the sidebar/header
+var temp_string3 = $('#login').html();
+var rendered_temp3 = _.template(temp_string3);
 
 
 //Inserting My Location into the sidebar
-var temp_string = $('#location').html();
-var rendered_temp = _.template(temp_string);
-
-
-  $('.my-location').append(rendered_temp(user));
+var temp_string4 = $('#location').html();
+var rendered_temp4 = _.template(temp_string4);
 
 
 //Inserting My Join Date into the sidebar
-var temp_string = $('#join-date').html();
-var rendered_temp = _.template(temp_string);
-
-
-  $('.my-join-date').append(rendered_temp(user));
+var temp_string5 = $('#join-date').html();
+var rendered_temp5 = _.template(temp_string5);
 
 
 //Inserting Followers into the sidebar
-var temp_string = $('#followers').html();
-var rendered_temp = _.template(temp_string);
+var temp_string6 = $('#followers').html();
+var rendered_temp6 = _.template(temp_string6);
 
-
-  $('.followers').append(rendered_temp(user));
 
 //Inserting Following into the sidebar
-var temp_string = $('#following').html();
-var rendered_temp = _.template(temp_string);
+var temp_string7 = $('#following').html();
+var rendered_temp7 = _.template(temp_string7);
+
+//Inserting My Avatar into the header
+var temp_string8 = $('#avatar1').html();
+var rendered_temp8 = _.template(temp_string8);
 
 
-  $('.following').append(rendered_temp(user));
+///*** USER.JS FILE FUNCTION ***///
+
+var test = $.getJSON(url).done(function(user_data){
+//enter in here as many statements that pertain to this 'user_data' as possible
+
+// example~~~~> $('.hero-unit').append(rendered(user_data));
+$('.avatar').append(rendered_temp1(user_data));
+$('.my-name').append(rendered_temp2(user_data));
+$('.username').append(rendered_temp3(user_data));
+$('.my-location').append(rendered_temp4(user_data));
+$('.my-join-date').append(rendered_temp5(user_data));
+$('.followers').append(rendered_temp6(user_data));
+$('.following').append(rendered_temp7(user_data));
+$('.avatar1').append(rendered_temp8(user_data));
+
+});
